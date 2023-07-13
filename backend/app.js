9ac117e7f813924d37fd08ca145acf7b40a636ba
Oksana Shishova <oksi-love@yandex.ru>
2023-07-13 16:30:32 +0300

    15

--- backend/app.js
+++ backend/app.js
@@ -5,17 +5,14 @@ const helmet = require('helmet');
 const { errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
 const allRouters = require('./routes/router');
-const cors = require('./middlewares/cors');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors({
-  credentials: true,
-  origin: ['https://oksifoxy.mesto.nomoredomains.work'],
-}));
+app.use(cors);
 // AntiDOS & helmet
 // https://www.npmjs.com/package/express-rate-limit
 const limiter = rateLimit({
