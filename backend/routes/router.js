39d4735609488024affcc8b20a796efed94c60c8
Oksana Shishova <oksi-love@yandex.ru>
2023-07-16 12:35:59 +0000

    15

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -6,10 +6,10 @@ const NotFound = require('../errors/NotFound');
 const { createUser, login } = require('../controllers/users');
 const { validLogin, validCreateUser } = require('../utils/validate');
 
-router.use('/users', auth, userRoutes);
-router.use('/cards', auth, cardRoutes);
 router.use('/signin', validLogin, login);
 router.use('/signup', validCreateUser, createUser);
+router.use('/cards', auth, cardRoutes);
+router.use('/users', auth, userRoutes);
 router.use('/*', auth, (req, res, next) => {
   next(new NotFound('404: Страница не найдена.'));
 });
